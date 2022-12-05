8052907bc1e8dec7e15b3b5ce7a861729d8dd1a8
Alina Halushko <alinahalushko2@gmail.com>
2022-12-05 22:54:22 +0200

    pretired code and updated package json

--- src/components/Statistics/Statictics.jsx
+++ src/components/Statistics/Statictics.jsx
@@ -3,17 +3,23 @@ import css from './Statictics.module.css';
 import PropTypes from 'prop-types';
 import { Section } from 'components/Section/Section';
 
-export const Statictics = ({good, neutral, bad, total, positivePercentage}) => {
-    return (
-      <Section title={"Statistics"}>
-       <div className={css.item}>Good: {good}</div>
-       <div className={css.item}>Bad: {bad}</div>
-       <div className={css.item}>Neutral: {neutral}</div>
-       <div className={css.item}>Total: {total}</div>
-       <div className={css.item}>Positive percentage: {positivePercentage}%</div>
-      </Section>
-    );
-}
+export const Statictics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <Section title={'Statistics'}>
+      <div className={css.item}>Good: {good}</div>
+      <div className={css.item}>Bad: {bad}</div>
+      <div className={css.item}>Neutral: {neutral}</div>
+      <div className={css.item}>Total: {total}</div>
+      <div className={css.item}>Positive percentage: {positivePercentage}%</div>
+    </Section>
+  );
+};
 
 Statictics.propTypes = {
   good: PropTypes.number.isRequired,
@@ -21,5 +27,4 @@ Statictics.propTypes = {
   bad: PropTypes.number.isRequired,
   total: PropTypes.number.isRequired,
   positivePercentage: PropTypes.number.isRequired,
-
-}
+};
